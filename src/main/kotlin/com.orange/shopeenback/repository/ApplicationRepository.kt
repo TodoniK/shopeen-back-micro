1a89e81a5e1ee5d385df3429246024db95444e12
Jules Royet <jules.royet@orange.com>
2024-01-11 11:37:13 +0100

    :art: Ajout de l'entité Application (model, service, repo, dto).

--- src/main/kotlin/com.orange/shopeenback/repository/ApplicationRepository.kt
+++ src/main/kotlin/com.orange/shopeenback/repository/ApplicationRepository.kt
@@ -1,13 +1,10 @@
 package com.orange.shopeenback.repository
 
-import io.micronaut.data.mongodb.annotation.MongoFindQuery
 import io.micronaut.data.mongodb.annotation.MongoRepository
 import io.micronaut.data.repository.CrudRepository
 import org.bson.types.ObjectId
-import com.orange.shopeenback.dto.ApplicationDTO
+import com.orange.shopeenback.model.Application
 
 @MongoRepository
-interface ApplicationRepository : CrudRepository<ApplicationDTO, ObjectId>{
-    @MongoFindQuery("{idExtName: :name}")
-    fun findApplicationDTOByIdExtName(name: String): List<ApplicationDTO>
+interface ApplicationRepository : CrudRepository<Application, ObjectId>{
 }
